5f83f3ba07b569557690239bf2584c548bab6c1d
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2024-05-02 18:14:22 -0600

    se agregaron funciones para mayor interactividad en la app

---
+++ app/public/js/main.js
@@ -0,0 +1,54 @@
+function changeMenu() {
+	const btnMenu = document.getElementById('menu');
+	const optionsMenu = document.getElementById('menuOptions');
+	if (btnMenu.checked) {
+		optionsMenu.style.display = "block";
+	} else {
+		optionsMenu.style.display = "none";
+	}
+}
+
+function changeImg() {
+	const defaultImg = '/app/public/img/userDefault.png';
+	const imgPP = document.getElementById('imgPP');
+	const previewimg = document.getElementById('previewImgPP');
+	const imgInput = document.getElementById('img');
+	imgInput.addEventListener('change', e => {
+		if (e.target.files[0]) {
+			const reader = new FileReader();
+			reader.onload = function (e) {
+				previewimg.src = e.target.result;
+				previewimg.style.display = 'block';
+				imgPP.style.display = 'none';
+			}
+			reader.readAsDataURL(e.target.files[0]);
+		} else {
+			previewimg.style.display = 'none';
+			previewimg.src = defaultImg;
+			imgPP.style.display = 'block';
+		}
+	})
+}
+
+function activateInput(inputNumber) {
+	switch (inputNumber) {
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+
+		default:
+			break;
+	}
+}
