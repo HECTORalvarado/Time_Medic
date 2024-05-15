289a0165cc6a5222e73a84962dfa757e3f718c85
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2024-05-15 11:18:21 -0600

    administración de las citas

--- app/controllers/citasController.php
+++ app/controllers/citasController.php
@@ -15,17 +15,30 @@ class CitasController {
 	public function getCitasPaciente() {
 		return $this->citasModel->getCitas();
 	}
+
 	public function getCitasDoctor() {
 		return $this->citasModel->getCitasDoc();
 	}
+
 	public function cancelCita($id) {
+		$this->citasModel->cancelCita($id);
+		header("Location: /app/public/adminCitas.php");
+		die();
+	}
+
+	public function completeCita ($id) {
 		$this->citasModel->completeCita($id);
 		header("Location: /app/public/adminCitas.php");
 		die();
 	}
-	public function completeCita ($id) {
-		$this->citasModel->cancelCita($id);
-		header("Location: /app/public/adminCitas.php");
+	
+	public function getDoctors (){
+		return $this->citasModel->getAllDoctors();
+	}
+
+	public function addCita ($idDoctor, $fecha, $hora){
+		$this->citasModel->addCitas($idDoctor,$fecha,$hora);
+		header("Location: /app/public/getCitas.php");
 		die();
 	}
 }
