d0ff0db0ef4f69115f33fe35c420ad424b10c4d3
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2024-05-14 21:25:49 -0600

    citas realizadas

---
+++ app/controllers/completeCita.php
@@ -0,0 +1,21 @@
+<?php
+	//require_once '../../config/conn.php';
+	require_once '../controllers/citasController.php';
+	
+	// Se crea un nuevo controllador
+	$fcitastrl = new CitasController();
+	
+	// Verifica si se ha enviado un formulario
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		// Obtiene el username y el password del formulario
+		
+		$inputId = $_POST["id"];
+		
+		if (!empty($inputId)) {
+			$fcitastrl->completeCita($inputId);
+		} else {
+			echo "Formulario incompleto";
+		}
+	}
+?>
