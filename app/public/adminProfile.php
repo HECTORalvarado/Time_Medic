3b82275d44f3c32f934ef87195702a4c5a9e6b80
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2024-05-16 20:32:13 -0600

    mejoras en la forma de mostrar los datos del usuario

--- app/public/adminProfile.php
+++ app/public/adminProfile.php
@@ -27,7 +27,8 @@ $user = $userCtrl->getUserInfor();
 
 <body>
 	<main>
-		<form action="" class="grid3">
+		<form action="../controllers/editUser.php" method="post" class="grid3">
+			<input type="hidden" name="id" value=<?php $user['idusuario'] ?>>
 			<section>
 				<figure class="imgContainer imgAdminProfile">
 					<input type="file" name="img" id="img" accept="image/*">
@@ -35,12 +36,12 @@ $user = $userCtrl->getUserInfor();
 						<i class="fa-solid fa-pencil fa-2x btnPencil"></i>
 					</label>
 					<img src="./img/userDefault.png" alt="" id="previewImgPP">
-					<?php 
-						if($user['img'] != null){
-							echo'<img src="./img/userDefault.png" alt="" id="imgPP">';
-						} else{
-							echo '<img src="./img/userDefault.png" alt="" id="imgPP">';
-						}
+					<?php
+					if ($user['img'] != null) {
+						echo '<img src="data:image/jpg;base64,', base64_encode($user['img']), '" alt="" id="imgPP">';
+					} else {
+						echo '<img src="./img/userDefault.png" alt="" id="imgPP">';
+					}
 					?>
 				</figure>
 			</section>
@@ -49,53 +50,41 @@ $user = $userCtrl->getUserInfor();
 				<h4><?php echo $_SESSION['username']; ?></h4>
 				<div class="grid2Form">
 					<div>
-						<div>
-							<input type="text" name="name" value=<?php echo $user['nombre'];?> placeholder="nombre" disabled required class="profileInput">
-							<button onclick="activateInput(1)"><i class="fa-solid fa-pencil btnPencil"></i></button>
+						<div class="grid2Input">
+							<input type="text" name="name" id="name" value=<?php echo $user['nombre']; ?> placeholder="nombre" disabled required class="profileInput">
+							<button type="button" class="btnInput" onclick="activateInput(1)"><i class="fa-solid fa-pencil btnPencil"></i></button>
 						</div>
-						<div>
-							<input type="text" name="lastname" value=<?php echo $user['apellido'];?> placeholder="apellido" disabled required class="profileInput">
-							<button onclick="activateInput(2)"><i class="fa-solid fa-pencil btnPencil"></i></button>
+						<div class="grid2Input">
+							<input type="text" name="lastname" id="lastname" value=<?php echo $user['apellido']; ?> placeholder="apellido" disabled required class="profileInput">
+							<button type="button" class="btnInput" onclick="activateInput(2)"><i class="fa-solid fa-pencil btnPencil"></i></button>
 						</div>
 					</div>
 					<div>
-						<div>
-							<input type="text" name="username" value=<?php echo $user['username'];?> placeholder="username" disabled required class="profileInput">
-							<button onclick="activateInput(3)"><i class="fa-solid fa-pencil btnPencil"></i></button>
+						<div class="grid2Input">
+							<input type="text" name="username" id="username" value=<?php echo $user['username']; ?> placeholder="username" disabled required class="profileInput">
+							<button type="button" class="btnInput" onclick="activateInput(3)"><i class="fa-solid fa-pencil btnPencil"></i></button>
 						</div>
-						<div>
-							<input type="email" name="email" value=<?php echo $user['correo'];?> placeholder="email@example.com" disabled required class="profileInput">
-							<button onclick="activateInput(4)"><i class="fa-solid fa-pencil btnPencil"></i></button>
+						<div class="grid2Input">
+							<input type="email" name="email" id="email" value=<?php echo $user['correo']; ?> placeholder="email@example.com" disabled required class="profileInput">
+							<button type="button" class="btnInput" onclick="activateInput(4)"><i class="fa-solid fa-pencil btnPencil"></i></button>
 						</div>
-						<div>
-							<input type="password" name="password" placeholder="contraseña" disabled required class="profileInput">
-							<button onclick="activateInput(5)"><i class="fa-solid fa-pencil btnPencil"></i></button>
+						<div class="grid2Input">
+							<input type="password" name="password" id="password" placeholder="contraseña" disabled required class="profileInput">
+							<button type="button" class="btnInput" onclick="activateInput(5)"><i class="fa-solid fa-pencil btnPencil"></i></button>
 						</div>
 					</div>
 				</div>
 				<div class="grid2Form">
-					<button type="submit">
-						Guardar Cambios
-						<i class="fa-solid fa-floppy-disk"></i>
-					</button>
-					<?php
-					if ($_SESSION['role'] == 1) {
-						echo '<a href="/app/public/getCitas.php" class="canceLink">Cancelar
-							<i class="fa-solid fa-trash"></i>
-							</a>';
-					}
-					if ($_SESSION['role'] == 2) {
-						echo '<a href="/app/public/getCitas.php" class="canceLink">Cancelar
-							<i class="fa-solid fa-trash"></i>
-							</a>';
-					}
-					if ($_SESSION['role'] == 3) {
-						echo '<a href="/app/public/getCitas.php" class="canceLink">
-							Cancelar
-							<i class="fa-solid fa-trash"></i>
-							</a>';
-					}
-					?>
+					<div>
+						<button type="submit" name="edit" class="btnProfile">
+							Guardar Cambios &nbsp; <i class="fa-solid fa-floppy-disk"></i>
+						</button>
+					</div>
+					<div>
+						<button type="submit" name="delete" class="canceLink btnCancel">
+							Eliminar Cuenta &nbsp; <i class="fa-solid fa-trash"></i>
+						</button>
+					</div>
 				</div>
 			</section>
 		</form>
